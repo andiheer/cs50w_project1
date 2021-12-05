f1e10352e0c66c07923b7d52f4b5e844ce91f0c3
Andi Heer <andiheer@gmail.com>
2021-12-05 10:13:09 +0100

    Edited forms function

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     # Entry view
     path('wiki/<str:title>', views.view_entry, name="entry"),
-    path('edit/<str:title>', views.edit_entry, name="edit entry"),
+    path('edit_entry/<str:title>', views.edit_entry, name="edit entry"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.utils.html import format_html
 
-from . import util, forms 
-
+from . import util 
+from .forms import EditForm
 
 from markdown2 import Markdown
 
@@ -30,23 +30,28 @@ def edit_entry(request,title):
     # Form handling: GET or POST?
     if request.method == 'POST':
         form_entry = EditForm(request.POST)
-        if form_entry.is_valid():
+       # if form_entry.is_valid():
 
 
     # GET: show form
     else:
         # Get existing entry if exists
+        form_edit= EditForm()
         if title == "new_entry":
             content = ""
             title = ""
         else:
             content = util.get_entry(title)
-        if content == "None":
-            content = ""
-        form_edit= EditForm(request,title,content)
+            if content == "None":
+                content = ""
+            form_edit.form_title = title
+            form_edit.form_content = content        
+
 
         return render(request, "encyclopedia/edit_entry.html", {
-            "title": title,
-            "content": content
+            'form': form_edit,
+            'title': title
         })
+
+        # return render(request, 'name.html', {'form': form})
     
